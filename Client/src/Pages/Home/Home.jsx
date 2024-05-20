e18cd0f8e7e6496708fb455b85158677cc33c08d
ahnafhemal <ahnafhemal17@gmial.com>
2024-05-20 12:32:06 +0600

    hi

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id explicabo ad voluptatum aperiam dolorum voluptatem omnis impedit in! Excepturi quos iste praesentium dolorem consequuntur dolor dolorum quae atque iusto?
+        <h1>Hi</h1>
         </div>
     );
 };
